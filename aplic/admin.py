0a966e7b6fe3c8cffdec971a74cc1dac2de2df9b
kaslufl <leallucasfonseca@gmail.com>
2021-03-26 01:28:30 -0300

    fix: corrige modelos e registradores adiciona configurações no settings.py

--- aplic/admin.py
+++ aplic/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
-
-# Register your models here.
-@admin.register(Professor)
-class ProfessorAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'endereco', 'matricula', 'cnh', 'digital', 'tipo')
+from .models import Aluno, Professor, Conteudo, Turma
 
 
 @admin.register(Aluno)
 class AlunoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'endereco', 'matricula', 'cnh', 'digital', 'tipo', 'tipo_cnh', 'estado')
+    list_display = ('nome', 'matricula', 'cnh', 'situacao', 'categoria')
+
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'matricula', 'cnh', 'especializacao')
+
+
+@admin.register(Conteudo)
+class ConteudoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'carga_horaria')
 
 
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
-    list_display = ('max_alunos')
+    list_display = ('codigo', 'max_aluno', 'conteudo', 'professor')
--- aplic/models.py
+++ aplic/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 
+
 class Pessoa(models.Model):
-    OPCOES1 = (
-        ('Possui', 'Possui')
-        ('Não possui', 'Não possui')
+    OPCOES_CNH = (
+        ('Não possui', 'Não possui'),
+        ('Categoria A', 'Categoria A'),
+        ('Categoria B', 'Categoria B'),
+        ('Categoria C', 'Categoria C'),
+        ('Categoria D', 'Categoria D'),
+        ('Categoria E', 'Categoria E'),
+        ('ACC', 'ACC'),
     )
     nome = models.CharField('Nome', max_length=100)
+    data_nascimento = models.DateField('Data de Nascimento', help_text='Formato DD/MM/AAAA')
     cpf = models.CharField('CPF', max_length=15)
     endereco = models.CharField('Endereço', max_length=100)
-    matricula = models.IntegerField('Matricula', unique=True)
-    cnh = models.CharField('CNH', max_length=20, choices=OPCOES1)
-    digital = models.CharField('Digital', max_length=20, choices=OPCOES1)
+    matricula = models.IntegerField('Matrícula', unique=True)
+    cnh = models.CharField('CNH', max_length=20, choices=OPCOES_CNH)
 
     class Meta:
         abstract = True
@@ -18,38 +24,72 @@ class Pessoa(models.Model):
     def __str__(self):
         return self.nome
 
+
 class Aluno(Pessoa):
-    OPCOESCNH = (
-        ('Tipo A', 'Tipo A')
-        ('Tipo B', 'Tipo B')
-        ('Tipo C', 'Tipo C')
+    OPCOES_SITUACAO = (
+        ('1ª Habilitação', '1ª Habilitação'),
+        ('Renovação', 'Renovação'),
+        ('CRCI', 'CRCI'),
+    )
+    OPCOES_CATEGORIA = (
+        ('Categoria A', 'Categoria A'),
+        ('Categoria B', 'Categoria B'),
+        ('Categoria C', 'Categoria C'),
+        ('Categoria D', 'Categoria D'),
+        ('Categoria E', 'Categoria E'),
+        ('ACC', 'ACC'),
     )
 
-    #ESTADO = () verificar o estado que o aluno ira possuir
+    situacao = models.CharField('Situação da Carteira',
+                                max_length=20,
+                                choices=OPCOES_SITUACAO)
 
-    tipo_cnh = models.CharField('Tipo de CNH', max_length=20, choices=OPCOESCNH)
-    estado = models.CharField("Estado", max_length=20)
+    categoria = models.CharField('Categoria',
+                                 max_length=20,
+                                 choices=OPCOES_CATEGORIA)
 
     class Meta:
         verbose_name = 'Aluno'
         verbose_name_plural = 'Alunos'
 
+
 class Professor(Pessoa):
-
-    #TIPO = () verificar o tipo que o professor ira possuir
-
-    tipo = models.CharField("Tipo", max_length=20)
+    OPCOES_ESPECIALIZACAO = (
+        ('Instrutor Prático', 'Instrutor Prático'),
+        ('Instrutor Teórico', 'Instrutor Teórico'),
+    )
+    especializacao = models.CharField('Especialização',
+                                      max_length=20,
+                                      choices=OPCOES_ESPECIALIZACAO)
 
     class Meta:
         verbose_name = 'Professor'
         verbose_name_plural = 'Professores'
 
-class Turma(models.Model):
 
+class Conteudo(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    descricao = models.TextField('Descrição', max_length=500)
+    carga_horaria = models.IntegerField('Carga Horária')
+
+    class Meta:
+        verbose_name = 'Conteúdo'
+        verbose_name_plural = 'Conteúdos'
+
+    def __str__(self):
+        return self.nome
+
+
+class Turma(models.Model):
+    codigo = models.CharField('Código', max_length=10)
     max_aluno = models.IntegerField('Quantidade de alunos por turma')
     professor = models.ForeignKey(Professor, null=True, on_delete=models.SET_NULL)
+    conteudo = models.ForeignKey(Conteudo, on_delete=models.CASCADE)
     alunos = models.ManyToManyField(Aluno)
 
     class Meta:
         verbose_name = 'Turma'
         verbose_name_plural = 'Turmas'
+
+    def __str__(self):
+        return f"{self.codigo} - {self.conteudo} - {self.max_aluno}"
