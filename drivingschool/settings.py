3edb6e533ace72003f4cb8617f5f0014507b7fd2
kaslufl <leallucasfonseca@gmail.com>
2021-03-26 21:52:27 -0300

    fix: corrige settings.py

--- drivingschool/settings.py
+++ drivingschool/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import dj_database_url
 
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=toibi-#e9g=sx_(h2txpyda(ozp0yi5q_d76k574d@p!m%tmz'
+SECRET_KEY = os.getenv("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
