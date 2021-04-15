1935b49969e3f289d4941589cc95c666d0bf04fe
Guilherme <guilhermedms@hotmail.com>
2021-04-15 07:39:26 -0300

    adição dos frond e de fotos e rede sociais no professor

--- aplic/views.py
+++ aplic/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from aplic.models import Professor
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['professores'] = Professor.objects.order_by('nome').all()
+        return context
