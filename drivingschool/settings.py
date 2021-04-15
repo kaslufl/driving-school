cbda27eb32aba85fb9942a3feccba550ff74efd6
kaslufl <leallucasfonseca@gmail.com>
2021-04-15 17:05:18 -0300

    heroku

--- drivingschool/settings.py
+++ drivingschool/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("KEY")
+SECRET_KEY = '=toibi-#e9g=sx_(h2txpyda(ozp0yi5q_d76k574d@p!m%tmz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
