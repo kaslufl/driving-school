3b97ba4b0da12fb6be714b0929f9f9ea4edb9c8a
Guilherme <guilhermedms@hotmail.com>
2021-06-08 09:15:29 -0300

    coreção do formulario de envio de email

--- aplic/urls.py
+++ aplic/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from .api.v1.router import router as v1
-from .views import IndexView
+from .views import IndexView, ContatoView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('conato/', ContatoView.as_view(), name='contato'),
     path('api/v1/', include(v1.urls)),
     path('api/v1/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- aplic/views.py
+++ aplic/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView
 from django.utils.translation import gettext as _
 
 from aplic.forms import ContatoForm
@@ -16,12 +17,21 @@ class IndexView(TemplateView):
         context['professores'] = Professor.objects.order_by('nome').all()
         return context
 
+
+class ContatoView(FormView):
+    template_name = 'contato.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('contato')
+
+    def get_context_data(self, **kwargs):
+        context = super(ContatoView, self).get_context_data(**kwargs)
+        return context
+
     def form_valid(self, form, *args, **kwargs):
         form.send_mail()
         messages.success(self.request, _('E-mail enviado com sucesso'), extra_tags='success')
-        return super(IndexView, self).form_valid(form, *args, **kwargs)
+        return super(ContatoView, self).form_valid(form, *args, **kwargs)
 
     def form_invalid(self, form, *args, **kwargs):
         messages.error(self.request, _('Falha ao enviar e-mail'), extra_tags='danger')
-        return super(IndexView, self).form_invalid(form, *args, **kwargs)
-
+        return super(ContatoView, self).form_invalid(form, *args, **kwargs)
